3a7b27c7236a55ad274a09c4d438f44ed5d8fba2
Alexmarc <alex.marchetti.graphisme@gmail.com>
2024-05-15 10:26:19 +0200

    changes on the way used to get identical files

--- py_CompareFolders.py
+++ py_CompareFolders.py
@@ -3,41 +3,45 @@ import filecmp
 from pathlib import Path
 
 def list_files(directory):
-    """ Renvoie une liste des chemins de fichiers dans le répertoire donné. """
-    return {file for file in Path(directory).rglob('*') if file.is_file()}
+    """ Renvoie un dictionnaire des fichiers dans le répertoire donné, où la clé est le nom du fichier et la valeur est le chemin complet. """
+    return {file.name: file for file in Path(directory).rglob('*') if file.is_file()}
 
 def compare_folders(folder1, folder2):
     # Récupérer les fichiers de chaque dossier
     files1 = list_files(folder1)
     files2 = list_files(folder2)
 
+    # Noms de fichiers dans chaque dossier
+    names1 = set(files1.keys())
+    names2 = set(files2.keys())
+
     # Fichiers uniquement dans chaque dossier
-    only_in_folder1 = files1 - files2
-    only_in_folder2 = files2 - files1
+    only_in_folder1 = names1 - names2
+    only_in_folder2 = names2 - names1
 
     # Fichiers communs aux deux dossiers
-    common_files = files1.intersection(files2)
+    common_names = names1 & names2
 
     # Comparer le contenu des fichiers communs
     same_files = []
     diff_files = []
-    for file in common_files:
-        # On compare les fichiers qui ont le même chemin
-        if filecmp.cmp(file, Path(folder2) / file.name, shallow=False):
-            same_files.append(file.name)
+    for name in common_names:
+        file1 = files1[name]
+        file2 = files2[name]
+        if filecmp.cmp(file1, file2, shallow=False):
+            same_files.append(name)
         else:
-            diff_files.append(file.name)
+            diff_files.append(name)
 
     return only_in_folder1, only_in_folder2, same_files, diff_files
 
-# Utiliser la fonction pour comparer deux dossiers
-folder_path1  = 'D:/AlexandreMarchettiDossierPersonnel/__GITHUB_PROJECT__/Utils_FilesModifications/CompareFolderTest/Test1'
-folder_path2  = 'D:/AlexandreMarchettiDossierPersonnel/__GITHUB_PROJECT__/Utils_FilesModifications/CompareFolderTest/Test2'
-
+# Chemins des deux dossiers à comparer
+folder_path1 = 'D:/AlexandreMarchettiDossierPersonnel/__GITHUB_PROJECT__/Utils_FilesModifications/CompareFolderTest/Test1'
+folder_path2 = 'D:/AlexandreMarchettiDossierPersonnel/__GITHUB_PROJECT__/Utils_FilesModifications/CompareFolderTest/Test2'
 
 # Appel de la fonction de comparaison
 results = compare_folders(folder_path1, folder_path2)
-print("Fichiers uniquement dans ", {folder_path1}, ":", {f.name for f in results[0]})
-print("Fichiers uniquement dans ", {folder_path2}, ":", {f.name for f in results[1]})
+print("Fichiers uniquement dans", folder_path1, ":", results[0])
+print("Fichiers uniquement dans", folder_path2, ":", results[1])
 print("Fichiers identiques dans les deux dossiers:", results[2])
 print("Fichiers avec des différences:", results[3])
